28c524b89a0de594091bd1d096d6ca7b21ee10e8
lberrocal <luisberrocal@outlook.com>
2020-09-06 22:17:48 -0400

    Creating Empty main class

---
+++ GenericProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenericProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
